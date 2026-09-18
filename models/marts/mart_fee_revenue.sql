8a24699d8eb59f4133091d8bddfc25b7e9e39572
Krishuang-1116 <natsuno1116@icloud.com>
2026-09-18 21:25:29 +0200

    add model contracts on fact_deal_snapshot and mart_fee_revenue, dbt exposure in v2 polish

--- models/marts/mart_fee_revenue.sql
+++ models/marts/mart_fee_revenue.sql
@@ -1,5 +1,5 @@
-{{config(materialized='incremental',incremental_strategy='merge',unique_key='invoice_id') }}
-SELECT 
-* 
+{{config(materialized='incremental',incremental_strategy='merge',unique_key='invoice_id',on_schema_change='fail') }}
+SELECT
+*
 FROM {{ref ('int_fee_invoice')}}
 {% if is_incremental() %} WHERE source_extract_timestamp > (SELECT MAX(source_extract_timestamp) FROM {{ this }}) {% endif %}  
