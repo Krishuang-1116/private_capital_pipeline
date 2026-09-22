16754eb87e5e540cd709576cfa85bacb3c94155d
Krishuang-1116 <natsuno1116@icloud.com>
2026-09-22 21:34:20 +0200

    add airflow dag and fixed one config in mart_fee_revenue, the incremental model

--- models/marts/mart_fee_revenue.sql
+++ models/marts/mart_fee_revenue.sql
@@ -1,4 +1,4 @@
-{{config(materialized='incremental',incremental_strategy='merge',unique_key='invoice_id',on_schema_change='fail') }}
+{{config(materialized='incremental',incremental_strategy='merge',unique_key='invoice_id',on_schema_change='append_new_columns') }}
 SELECT
 *
 FROM {{ref ('int_fee_invoice')}}
